36eab23089acb78d718ae1e785f8fdf2d4b192d1
extremeYes <92187320+extremeYes@users.noreply.github.com>
2026-08-04 17:02:19 -0300

    Embedded headers file

--- src/game.h
+++ src/game.h
@@ -12,7 +12,45 @@
 //////
 ///
 
-#include "headers_and_types.h"
+////////////////
+/// headers
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <raylib.h>
+#include <raymath.h>
+
+#define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
+#include "../nob.h"
+
+////////////////
+/// Types
+
+#define bool  _Bool
+#define true  1
+#define false 0
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+
+typedef size_t    usize; // Unsigned, pointer-sized
+typedef ptrdiff_t isize; // Signed, pointer-sized
+
+typedef float  f32;   // IEEE 754 Single Precision
+typedef double f64;   // IEEE 754 Double Precision
+
 
 /// Static Globals
 
